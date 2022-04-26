3997d714fa2808f5dd0b12215394a76e5a09b188
e.kaplina <e.kaplina@profsoft.pro>
2022-04-26 18:25:03 +0400

    add cors

--- revivl/config/bundles.php
+++ revivl/config/bundles.php
@@ -13,4 +13,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
