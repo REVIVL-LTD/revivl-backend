50d3feb9c3035fbd9a10cf1300b95505a35ebfca
Каплина Екатерина <e.kaplina@profsoft.pro>
2022-06-10 15:10:00 +0400

    add abbreviation Course

--- revivl/src/Entity/Course.php
+++ revivl/src/Entity/Course.php
@@ -8,6 +8,8 @@ use App\Repository\CourseRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 #[ORM\Entity(repositoryClass: CourseRepository::class)]
 #[ORM\Table(name: 'course')]
@@ -24,8 +26,12 @@ class Course
     #[ORM\Column(type: 'string', length: 255)]
     private $name;
 
-    #[ORM\Column(type: 'string', length: 255)]
+    #[ORM\Column(type: 'text')]
     private $description;
+
+    #[ORM\Column(type: 'string', length: 10)]
+    #[Assert\Length(max:10)]
+    private $abbreviation;
 
     #[ORM\Column(type: 'integer')]
     private $cost;
@@ -110,9 +116,22 @@ class Course
     {
         return $this->cost;
     }
+
     public function setCost(int $cost): self
     {
         $this->cost = $cost;
+
+        return $this;
+    }
+
+    public function getAbbreviation(): string
+    {
+        return $this->abbreviation;
+    }
+
+    public function setAbbreviation($abbreviation): self
+    {
+        $this->abbreviation = $abbreviation;
 
         return $this;
     }
