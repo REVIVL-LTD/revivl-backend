0790c70d3f9f0eccc110ad01f9ce2f3411372f72
Каплина Екатерина <e.kaplina@profsoft.pro>
2022-06-10 10:22:12 +0400

    many to one migration

--- revivl/migrations/Version20220610061946.php
+++ revivl/migrations/Version20220610061946.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220601194218 extends AbstractMigration
+final class Version20220610061946 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -24,34 +24,46 @@ final class Version20220601194218 extends AbstractMigration
         $this->addSql('CREATE SEQUENCE code_auth_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE course_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE course_user_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE "order_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE promocode_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE reset_password_request_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE "user_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE address (id INT NOT NULL, post_code VARCHAR(255) NOT NULL, address_line VARCHAR(500) NOT NULL, city VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE admin (id INT NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE TABLE code_auth (id INT NOT NULL, code INT NOT NULL, email VARCHAR(255) NOT NULL, live_time TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE code_auth (id INT NOT NULL, patient_id INT DEFAULT NULL, code INT NOT NULL, live_time TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_CE35DA986B899279 ON code_auth (patient_id)');
         $this->addSql('CREATE TABLE course (id INT NOT NULL, doctor_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, cost INT NOT NULL, photo VARCHAR(255) DEFAULT NULL, status INT NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_169E6FB987F4FB17 ON course (doctor_id)');
         $this->addSql('CREATE TABLE course_user (id INT NOT NULL, course_id INT DEFAULT NULL, patient_id INT DEFAULT NULL, status INT NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_45310B4F591CC992 ON course_user (course_id)');
         $this->addSql('CREATE INDEX IDX_45310B4F6B899279 ON course_user (patient_id)');
         $this->addSql('CREATE TABLE doctor (id INT NOT NULL, surname VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, sex BOOLEAN NOT NULL, photo VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE "order" (id INT NOT NULL, patient_id INT DEFAULT NULL, status INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_F52993986B899279 ON "order" (patient_id)');
         $this->addSql('CREATE TABLE patient (id INT NOT NULL, address_id INT DEFAULT NULL, phone VARCHAR(255) NOT NULL, birthday DATE NOT NULL, surname VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, sex BOOLEAN NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_1ADAD7EBF5B7AF75 ON patient (address_id)');
         $this->addSql('CREATE TABLE promocode (id INT NOT NULL, name VARCHAR(255) NOT NULL, status INT NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE promocode_abstract_user (promocode_id INT NOT NULL, abstract_user_id INT NOT NULL, PRIMARY KEY(promocode_id, abstract_user_id))');
         $this->addSql('CREATE INDEX IDX_B8E4F00C76C06D9 ON promocode_abstract_user (promocode_id)');
         $this->addSql('CREATE INDEX IDX_B8E4F005D9B505 ON promocode_abstract_user (abstract_user_id)');
-        $this->addSql('CREATE TABLE "user" (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, status INT NOT NULL, type INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE reset_password_request (id INT NOT NULL, user_id INT NOT NULL, selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, expires_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_7CE748AA76ED395 ON reset_password_request (user_id)');
+        $this->addSql('COMMENT ON COLUMN reset_password_request.requested_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN reset_password_request.expires_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE "user" (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) DEFAULT NULL, status INT NOT NULL, type INT NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON "user" (email)');
         $this->addSql('ALTER TABLE admin ADD CONSTRAINT FK_880E0D76BF396750 FOREIGN KEY (id) REFERENCES "user" (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE code_auth ADD CONSTRAINT FK_CE35DA986B899279 FOREIGN KEY (patient_id) REFERENCES patient (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE course ADD CONSTRAINT FK_169E6FB987F4FB17 FOREIGN KEY (doctor_id) REFERENCES doctor (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE course_user ADD CONSTRAINT FK_45310B4F591CC992 FOREIGN KEY (course_id) REFERENCES course (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE course_user ADD CONSTRAINT FK_45310B4F6B899279 FOREIGN KEY (patient_id) REFERENCES patient (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE doctor ADD CONSTRAINT FK_1FC0F36ABF396750 FOREIGN KEY (id) REFERENCES "user" (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE "order" ADD CONSTRAINT FK_F52993986B899279 FOREIGN KEY (patient_id) REFERENCES patient (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE patient ADD CONSTRAINT FK_1ADAD7EBF5B7AF75 FOREIGN KEY (address_id) REFERENCES address (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE patient ADD CONSTRAINT FK_1ADAD7EBBF396750 FOREIGN KEY (id) REFERENCES "user" (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE promocode_abstract_user ADD CONSTRAINT FK_B8E4F00C76C06D9 FOREIGN KEY (promocode_id) REFERENCES promocode (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE promocode_abstract_user ADD CONSTRAINT FK_B8E4F005D9B505 FOREIGN KEY (abstract_user_id) REFERENCES "user" (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     public function down(Schema $schema): void
@@ -61,17 +73,22 @@ final class Version20220601194218 extends AbstractMigration
         $this->addSql('ALTER TABLE patient DROP CONSTRAINT FK_1ADAD7EBF5B7AF75');
         $this->addSql('ALTER TABLE course_user DROP CONSTRAINT FK_45310B4F591CC992');
         $this->addSql('ALTER TABLE course DROP CONSTRAINT FK_169E6FB987F4FB17');
+        $this->addSql('ALTER TABLE code_auth DROP CONSTRAINT FK_CE35DA986B899279');
         $this->addSql('ALTER TABLE course_user DROP CONSTRAINT FK_45310B4F6B899279');
+        $this->addSql('ALTER TABLE "order" DROP CONSTRAINT FK_F52993986B899279');
         $this->addSql('ALTER TABLE promocode_abstract_user DROP CONSTRAINT FK_B8E4F00C76C06D9');
         $this->addSql('ALTER TABLE admin DROP CONSTRAINT FK_880E0D76BF396750');
         $this->addSql('ALTER TABLE doctor DROP CONSTRAINT FK_1FC0F36ABF396750');
         $this->addSql('ALTER TABLE patient DROP CONSTRAINT FK_1ADAD7EBBF396750');
         $this->addSql('ALTER TABLE promocode_abstract_user DROP CONSTRAINT FK_B8E4F005D9B505');
+        $this->addSql('ALTER TABLE reset_password_request DROP CONSTRAINT FK_7CE748AA76ED395');
         $this->addSql('DROP SEQUENCE address_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE code_auth_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE course_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE course_user_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE "order_id_seq" CASCADE');
         $this->addSql('DROP SEQUENCE promocode_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE reset_password_request_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE "user_id_seq" CASCADE');
         $this->addSql('DROP TABLE address');
         $this->addSql('DROP TABLE admin');
@@ -79,9 +96,11 @@ final class Version20220601194218 extends AbstractMigration
         $this->addSql('DROP TABLE course');
         $this->addSql('DROP TABLE course_user');
         $this->addSql('DROP TABLE doctor');
+        $this->addSql('DROP TABLE "order"');
         $this->addSql('DROP TABLE patient');
         $this->addSql('DROP TABLE promocode');
         $this->addSql('DROP TABLE promocode_abstract_user');
+        $this->addSql('DROP TABLE reset_password_request');
         $this->addSql('DROP TABLE "user"');
     }
 }
